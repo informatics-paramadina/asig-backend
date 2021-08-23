f76764d24eadaeba92329aeb6c95efc120f284c2
ghifari-ti <muhammad.ghifari@students.paramadina.ac.id>
2021-08-23 16:55:54 +0700

    cobatest

--- controllers/project.controllers.js
+++ controllers/project.controllers.js
@@ -1,4 +1,4 @@
-const db = require("../config/database");
+const db = require("../config/database"); 
 
 const getProjects = (req, res, next) => {
     db.from('project').select().then(row => res.send(row)).catch(err => next(err));
